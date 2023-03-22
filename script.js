50460d103f721e0d65861d97ec5b83f629313561
Alexandr <alexandr_zubkov95@mail.ru>
2023-03-22 19:12:36 +0300

    Наш первый комит

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var number = 5; //числовой тип данных
+var string = "Hello"; // строчный тип данных
+var sym = Symbol(); // тип данных ( символ)
+var boolean = true; // булево значение 
+null; // отсутствующий тип данных
+undefined; // переменная не определена ( var , let )
+var obj = {}; // тип данных обьект
+                // type number "NaN" and "infinity"
+
+/* let persone = {
+    name: "John",   // пример обьекта
+    age: 25,
+    isMarried: false
+};
+console.log(persone.name);
+
+let arr = ['pulp.png', "frog.jpg", "vk.svg"];
+console.log(arr[1]);
+
+let answer = prompt("вы готовы?", "Да");
+
+console.log(typeof(arr)); */
+
+
+let incr = 10, decr = 10;
+
+console.log(8 == 24/3);
+
+let isCheced = false,
+    isClose = false;
+
+console.log();
